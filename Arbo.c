5a07e5977a553ab9dcfdbb7f5dda50ad8fd6a39f
starrit <starrit@student.42.fr>
2017-02-11 12:32:55 +0100

    arbo : add Makefile in routines, add real-test

--- Arbo.c
+++ Arbo.c
@@ -5,15 +5,23 @@ rendu :
 						srcs pour la lib
 
 		test 		: 
+						Makefile (?)
 						main.c
 						get_next_line : 
 										00_launcher.c
-										01_basic_test.c
-										02_hard_test.c
-										03_null_test.c
+										01_basic_succes_test.c
+										02_basic_error_test.c
+										03_segv_test.c
+										04_bus_test.c
 
 						autre_fontion :
 										00_launcher.c
 										01_basic_test.c
 										02_hard_test.c
 										03_null_test.c
+		real_tests : 
+						Makefile (sur)
+						fct_to_test	  :
+										00_launcher.c
+										01_to_15_tests.c
+
