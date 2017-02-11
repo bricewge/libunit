d8da7e99229ab9517d17588a7632ab9240b20fe3
Brice Waegeneire <brice.wge@gmail.com>
2017-02-11 15:48:49 +0100

    add framework/load_test.c

---
+++ framework/load_test.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_test.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwaegene <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/11 14:55:36 by bwaegene          #+#    #+#             */
+/*   Updated: 2017/02/11 15:41:19 by bwaegene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libunit.h"
+
+void		load_test(t_test *testlist, char *name, int (*f)(void))
+{
+	if (!name || !f)
+		return;
+	if (!testlist)
+		ft_create_test_lst(testlist);
+	else
+		ft_add_test_lst(testlist);
+	testlist->name = name;
+	testlist->fct = &f;
+}
