ae5c47c99a4744b3920761d0addb167b02784296
starrit <starrit@student.42.fr>
2017-02-11 15:02:12 +0100

    libunit.h : maj t_test + add print_header

--- framework/libunit.h
+++ framework/libunit.h
@@ -6,7 +6,7 @@
 /*   By: starrit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 12:46:15 by starrit           #+#    #+#             */
-/*   Updated: 2017/02/11 14:48:18 by bwaegene         ###   ########.fr       */
+/*   Updated: 2017/02/11 14:59:40 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 typedef struct			s_test
 {
 	int					res;
+	char				*name;
+	int (*f)(void)		fct;
 	struct test_struct	*next;
 }						t_test;
 /*
@@ -24,7 +26,10 @@ typedef struct			s_test
 void		ft_create_test_lst(test_lst *new_test);
 void		ft_add_test_lst(test_lst *base);
 void		ft_del_test_lst(test_lst *base);
-
+/*
+**			M_FRAMEWORK FUNCTIONS
+*/
+void		print_header(void);
 void		load_test(t_test *testlist, char *name, int (*f)(void));
 void		lauch_test(t_test *testlist, int *res_test);
 int			print_total(int *res_test);
