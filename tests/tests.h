f198aa37ef9500fd3e3af0e237d5b7bb78b085ba
starrit <starrit@student.42.fr>
2017-02-11 21:00:13 +0100

    test/tests.h : add test 03_abort

--- tests/tests.h
+++ tests/tests.h
@@ -6,14 +6,15 @@
 /*   By: bwaegene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 17:25:43 by bwaegene          #+#    #+#             */
-/*   Updated: 2017/02/11 17:30:10 by bwaegene         ###   ########.fr       */
+/*   Updated: 2017/02/11 20:55:20 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TESTS_H
 # define TESTS_H
-
+#include "libunit.h"
 void		strlen_launcher(int *res_test);
 int			basic_test(void);
 int			null_test(void);
+int			abort_test(void);
 #endif
