5e74102dbc1e017f07a2eb518207702c717af1c3
starrit <starrit@student.42.fr>
2017-02-11 12:52:50 +0100

    +lst struct

--- framework/libunit.h
+++ framework/libunit.h
@@ -6,10 +6,22 @@
 /*   By: starrit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 12:46:15 by starrit           #+#    #+#             */
-/*   Updated: 2017/02/11 12:46:33 by starrit          ###   ########.fr       */
+/*   Updated: 2017/02/11 12:52:07 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBUNIT_H
 # define LIBUNIT_H
+#include "libft/libft.h"
+typedef struct			test_struct
+{
+	char				*t_name;
+	char				*t_res;
+	struct test_struct	*next;
+}						test_lst;
+typedef struct			fct_struct
+{
+	struct				*test_struct;
+	struct	fct_struct	*next;
+}						fct_lst;
 #endif
