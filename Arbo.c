cb7b8f8baa42988c6879b5f5dd249f4656be97c7
starrit <starrit@student.42.fr>
2017-02-11 12:11:33 +0100

    arborescence

---
+++ Arbo.c
@@ -0,0 +1,18 @@
+rendu :
+		Makefile
+		framework	: 
+						srcs pour la lib
+
+		test 		: 
+						main.c
+						get_next_line : 
+										00_launcher.c
+										01_basic_test.c
+										02_hard_test.c
+										03_null_test.c
+
+						autre_fontion :
+										00_launcher.c
+										01_basic_test.c
+										02_hard_test.c
+										03_null_test.c
